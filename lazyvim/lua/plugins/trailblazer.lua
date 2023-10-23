b4783b2c754febd4aaeb78fbf5937399d2c2f818
Mjb141 <mike141@live.com>
2023-10-23 15:23:40 +0100

    Add trailblazer

---
+++ lazyvim/lua/plugins/trailblazer.lua
@@ -0,0 +1,15 @@
+return {
+  "LeonHeidelbach/trailblazer.nvim",
+  version = false,
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      nv = {
+        motions = {
+          new_trail_mark = "<leader>tm",
+          track_back = "<leader>tb",
+        },
+      },
+    },
+  },
+}
